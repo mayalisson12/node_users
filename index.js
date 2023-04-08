74f50904f2b1cbce085fc3ac7a684f19a78edae6
Mayalisson Barbosa Pereira <mayalisson@gmail.com>
2023-04-08 12:55:54 -0300

    api_users

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const uuid = require('uuid')
+
+const port = 3000
+const app = express()
+app.use(express.json())
+
+
+/**
+        - Query params => meusite.com/users?name=mayalisson$age=23 //Filtros
+        - Route params => /users/2      //BUSCAR, DELETAR OU ATUALIZAR ALGO ESPECIFICO
+
+ */
+
+/**
+ * Como Funciona as request via Route params
+ */
+
+const users = []
+
+
+app.get('/users', (request, response) => {
+    return response.json(users)
+})
+
+app.post('/users', (request, response) => {
+    const { name, age } = request.body
+
+    const user = { id: uuid.v4(), name, age }
+
+    users.push(user)
+
+    return response.status(201).json(user)
+
+})
+
+app.put('/users/:id', (request, response) => {
+
+    const { id } = request.params
+    const { name, age } = request.body
+
+    const updateUser = { id, name, age }
+
+    const index = users.findIndex(user => user.id === id)
+
+
+    if (index < 0) {
+        return response.status(404).json({ message: "User not found" })
+    }
+
+    console.log(index)
+
+    return response.json(users)
+})
+
+
+app.delete('/users/:id', (request, response) => {
+    
+
+    const { id } = request.params
+
+    const index = users.findIndex(user => user.id === id)
+
+
+    if (index < 0) {
+        return response.status(404).json({ message: "User not found" })
+    }
+
+    users.splice(index,1)
+
+
+
+
+    return response.status(204).json()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`server started on port ${port} 🚀`)
+})
